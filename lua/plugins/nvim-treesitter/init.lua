3e03a8f842cbe4ffd4352372496cdd0a57e43506
Yinbo Li <liyinbo4@gmail.com>
2023-11-02 13:56:53 +0800

    Remove all ensure installed treesitter parsers.

--- lua/plugins/nvim-treesitter/init.lua
+++ lua/plugins/nvim-treesitter/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = "all",
+  ensure_installed = {},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
